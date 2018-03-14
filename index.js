bff50525fbc3bb8086c84d1fd6d06aaa8f19268e
Matthew Kwong <mtkwong91@gmail.com>
2018-03-14 15:23:39 -0700

    moved health camp spa to node

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5555;
+const app = express();
+const { Client } = require('pg');
+const dbClient = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => res.render('pages/index'));
+
+app.get('/healthcampspa', function(req, res)) {
+  /*
+  dbClient.connect();
+
+  dbClient.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+      console.log(JSON.stringify(row));
+    }
+    dbClient.end();
+  });*/
+
+  res.render('pages/healthcampspa')
+}
+
+var server = app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+function stop() {
+  server.close();
+}
+
+module.exports = app;
+module.exports.stop = stop;
