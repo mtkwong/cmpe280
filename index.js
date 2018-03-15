521e061d9c37f6a9acc889555e12ed5f170aad33
Matthew Kwong <mtkwong91@gmail.com>
2018-03-15 13:53:21 -0700

    db test #07

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ app.get('/healthcamp', (req, res) => res.render('pages/health_camp_spa'));
 
 app.post('/savePersonalInfo', function(req, res) {
   console.log(req.body);
-  res.contentType('json');
-  res.send(JSON.stringify({response:'hello my world'}));
+  //res.contentType('json');
+  //res.send(JSON.stringify({response:'hello my world'}));
   /*
   dbClient.connect();
 
