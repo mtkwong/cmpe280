e9ab9a850d8384e7a7e4d725204d217fea7aa9f4
Matthew Kwong <mtkwong91@gmail.com>
2018-03-15 14:05:43 -0700

    db test 10

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const multer = require('multer');
+//const multer = require('multer');
 const PORT = process.env.PORT || 5555;
 const app = express();
 const { Client } = require('pg');
@@ -14,7 +14,7 @@ const dbClient = new Client({
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(multer());
+//app.use(multer());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => res.render('pages/index'));
