4f9bf86236bc89e6151148c49e4bb233e7093dcd
Matthew Kwong <mtkwong91@gmail.com>
2018-03-15 13:17:02 -0700

    db test #02

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const dbClient = new Client({
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false; }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => res.render('pages/index'));
