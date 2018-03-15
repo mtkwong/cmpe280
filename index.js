dcd468ab906d93a9a0696cc732e84d486c6285ea
Matthew Kwong <mtkwong91@gmail.com>
2018-03-15 16:53:28 -0700

    db test 16

--- index.js
+++ index.js
@@ -22,18 +22,22 @@ app.get('/healthcamp', (req, res) => res.render('pages/health_camp_spa'));
 
 app.post('/savePersonalInfo', function(req, res) {
   console.log(req.body);
+  const text = 'INSERT INTO healthrecords(ID, FirstName, LastName, Gender, Age, Details, Photo) VALUES($1, $2, $3, $4, $5, $6, $7)';
+  const values = [req.body.id, req.body.fn, req.body.ln, req.body.gn, req.body.ag, req.body.dt, req.body.ph];
   //res.contentType('json');
   //res.send(JSON.stringify({response:'hello my world'}));
-  /*
+  
   dbClient.connect();
-
-  dbClient.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-    if (err) throw err;
+  dbClient.query(text, values, (err, res) => {
+    if (err) {
+      console.log(err);
+    }
+    /*
     for (let row of res.rows) {
       console.log(JSON.stringify(row));
-    }
+    }*/
     dbClient.end();
-  });*/
+  });
 });
 
 app.post('/saveHealthInfo', function(req, res) {
