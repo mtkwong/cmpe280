9c79493eed49cfe6b45b805ec50683756eaef3db
Matthew Kwong <mtkwong91@gmail.com>
2018-03-24 15:31:23 -0700

    gp chatbot 02

---
+++ public/js/health_camp_spa.js
@@ -0,0 +1,146 @@
+var id=0; // Current ID, for inserting into database
+var pngUrl;
+
+function init() {
+  // Prepare camera capture
+  const player = document.getElementById('player');
+  const canvas = document.getElementById('canvas');
+  const context = canvas.getContext('2d');
+  const captureButton = document.getElementById('capture');
+
+  const constraints = {
+    video: true,
+  };
+
+  captureButton.addEventListener('click', () => {
+    // Draw the video frame to the hidden canvas
+    context.drawImage(player, 0, 0, canvas.width, canvas.height);
+    pngUrl = canvas.toDataURL();
+  });
+
+  // Attach the video stream to the video element and autoplay
+  navigator.mediaDevices.getUserMedia(constraints)
+    .then((stream) => {
+      player.srcObject = stream;
+    });
+}
+
+// Show the appropriate content depending on which menubar item is clicked
+function showDiv(name) {
+  if(name === "Demographics") {
+    document.getElementById("dmTab").className = "currentTab";
+    document.getElementById("hvTab").className = "";
+    document.getElementById("rpTab").className = "";
+    document.getElementById("dmDiv").style.display = "block";
+    document.getElementById("hvDiv").style.display = "none";
+    document.getElementById("rpDiv").style.display = "none";
+  }
+  else if(name === "Health Vitals") {
+    document.getElementById("dmTab").className = "";
+    document.getElementById("hvTab").className = "currentTab";
+    document.getElementById("rpTab").className = "";
+    document.getElementById("dmDiv").style.display = "none";
+    document.getElementById("hvDiv").style.display = "block";
+    document.getElementById("rpDiv").style.display = "none";
+  }
+  else if(name === "Reports") {
+    document.getElementById("dmTab").className = "";
+    document.getElementById("hvTab").className = "";
+    document.getElementById("rpTab").className = "currentTab";
+    document.getElementById("dmDiv").style.display = "none";
+    document.getElementById("hvDiv").style.display = "none";
+    document.getElementById("rpDiv").style.display = "block";
+    buildTable();
+  }
+}
+
+function saveDm() {
+  // Update ID here
+  id++;
+
+  $.ajax({
+    url: "/savePersonalInfo",
+    type: "POST",
+    data: JSON.stringify({
+      id: id,
+      fn: document.getElementById("fnm").value,
+      ln: document.getElementById("lnm").value,
+      gn: document.getElementById("gnd").value,
+      ag: document.getElementById("age").value,
+      dt: document.getElementById("det").value,
+      ph: pngUrl
+    }),
+    contentType: "application/json",
+  });
+
+  document.getElementById("dmForm").reset();
+}
+
+function saveHv() {
+  $.ajax({
+    url: "/saveHealthInfo",
+    type: "POST",
+    data: JSON.stringify({
+      id: id,
+      ht: document.getElementById("hgt").value,
+      wt: document.getElementById("wgt").value,
+      bt: document.getElementById("bdt").value,
+      pr: document.getElementById("pls").value,
+      bp: document.getElementById("bps").value,
+      md: document.getElementById("med").value,
+      nt: document.getElementById("nts").value
+    }),
+    contentType: "application/json",
+  });
+
+  document.getElementById("hvForm").reset();
+  document.getElementById("med").value = "";
+  document.getElementById("nts").value = "";
+}
+
+function buildTable() {
+  $.ajax({
+    url: "/retrieveInfo",
+    type: "GET",
+    success: function(res) {
+      var results = JSON.parse(res);
+      var len = results.length,i,item,tr,tdName,tdAge,tdGender,tdPhoto,img,tdMeds,tdNotes;
+      var tbody = document.getElementById("tbody");
+      tbody.innerHTML = "";
+
+      for (i = 0; i < len; i++){
+        item = results[i];
+        tr = document.createElement("tr");
+        if (i%2 == 0) {
+          tr.className = "even";
+        } else {
+          tr.className = "odd";
+        }
+        tdName = document.createElement("td");
+        tdName.innerHTML = item.firstname + " " + item.lastname;
+        tr.appendChild(tdName);
+        tdAge = document.createElement("td");
+        tdAge.innerHTML = item.age;
+        tr.appendChild(tdAge);
+        tdGender = document.createElement("td");
+        tdGender.innerHTML = item.gender;
+        tr.appendChild(tdGender);
+
+        tdPhoto = document.createElement("td");
+        img = document.createElement("img");
+        img.src = item.photo;
+        tdPhoto.appendChild(img);
+        tr.appendChild(tdPhoto);
+
+        tdMeds = document.createElement("td");
+        tdMeds.innerHTML = item.medications;
+        tr.appendChild(tdMeds);
+        tdNotes = document.createElement("td");
+        tdNotes.innerHTML = item.notes;
+        tr.appendChild(tdNotes);
+
+        tbody.appendChild(tr);
+      }
+    }
+  });
+}
---
+++ public/js/myco_map.js
@@ -0,0 +1,74 @@
+function init() {
+  var map = initMap();
+
+  $.ajax({
+    url: "/getMapData",
+    type: "GET",
+    success: function(res) {
+      var json = JSON.parse(res);
+      var locArr = json.locations.location;
+      var locTypes = {
+        "Distribution Facility": {
+          marker: "http://www.googlemapsmarkers.com/v1/fc382a/",
+          color: '#fc382a'
+        },
+        "HeadQuarters": {
+          marker: "http://www.googlemapsmarkers.com/v1/ffffff/",
+          color: '#ffffff'
+        },
+        "Call Center": {
+          marker: "http://www.googlemapsmarkers.com/v1/8ce0ff/",
+          color: '#8ce0ff'
+        },
+        "RetailLocation": {
+          marker: "http://www.googlemapsmarkers.com/v1/32d137/",
+          color: '#32d137'
+        }
+      }
+      var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+      var labelIndex = 0;
+      var legend = document.getElementById('legend');
+
+      for(var i=0; i<locArr.length; i++) {
+        var loc = locArr[i];
+        var pos = new google.maps.LatLng(loc.latitude, loc.longitude);
+        var curIcon = locTypes[loc.type].marker;
+        var curLabel = labels[labelIndex++ % labels.length];
+
+        var marker = new google.maps.Marker({
+          position: pos,
+          icon: curIcon,
+          label: curLabel,
+          map: map
+        });
+
+        var circle = new google.maps.Circle({
+          strokeColor: locTypes[loc.type].color,
+          strokeOpacity: 0.8,
+          strokeWeight: 2,
+          fillColor: locTypes[loc.type].color,
+          fillOpacity: 0.30,
+          map: map,
+          center: pos,
+          radius: Math.sqrt(loc.$revenue)
+        });
+
+        var legend = document.getElementById('legend');
+        var div = document.createElement('div');
+        div.innerHTML = '<img src="' + curIcon + '" height=20 width=14> ' + curLabel + ': ' + loc.address;
+        legend.appendChild(div);
+      }
+
+      map.controls[google.maps.ControlPosition.RIGHT_TOP].push(legend);
+    }
+  });   
+}
+
+function initMap() {
+  var center = {lat: 37.382093, lng: -122.001715};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 10,
+    center: center
+  });
+  return map;
+}
